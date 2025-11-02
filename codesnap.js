8b5f5275d154b4b2d32d26345d46356892660e1f
Eivind Simonsen <74544541+eivindsimonsen@users.noreply.github.com>
2025-11-02 18:58:41 +0100

    Updated some text and images

---
+++ codesnap.js
@@ -0,0 +1,8 @@
+const portfolio = {
+  name: "Eivind Simonsen",
+  age: 27,
+  email: "eivind.simonsen@gmail.com",
+  phone: "+47 941 71 075",
+  profession: "CRM developer",
+  workplace: "Cloud Connection"
+};
